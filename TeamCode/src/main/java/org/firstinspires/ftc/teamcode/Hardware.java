99dc6a7ad1de62b831c931da6a7e71270dbff148
heroineforever <hanaasiddiqui@yahoo.com>
2019-09-17 17:25:10 -0500

    Declared motors for Palloween robot

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Hardware.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Hardware.java
@@ -2,7 +2,7 @@ public class HardwareRobot {
 
     public DcMotor leftDrive, rightDrive;
 
-    public Servo liftServo;
+    public DcMotor horizontalLift, verticalLift;
 
 
     //Constructor
@@ -12,11 +12,14 @@ public class HardwareRobot {
         //Motors
         leftDrive = hwmp.dcMotor.get("Left Drive");
         rightDrive = hwmp.dcMotor.get("Right Drive");
+        horizontalLift = hwmp.dcMotor.get("Horizontal Lift");
+        verticalLift = hwmp.dcMotor.get("Vertical Left");
+
         //If you want to put the motor flipped
         rightDrive.setDirection(DcMotor.Direction.REVERSE);
 
         //Servos
-        liftServo = hwmp.servo.get("Lift Servo");
+        //liftServo = hwmp.servo.get("Lift Servo");
 
 
     }
@@ -24,13 +27,22 @@ public class HardwareRobot {
     //General Methods
     public void resetDriveEncoders() {
 
-        rightDrive.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER); //reset
+        rightDrive.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);//reset
+        leftDrive.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        horizontalLift.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        verticalLift.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
 
         rightDrive.setMode(DcMotor.RunMode.RUN_USING_ENCODER); //get ready for rerun
+        leftDrive.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        horizontalLift.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        verticalLift.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
     }
 
     public void stopAllMotors() {
         rightDrive.setPower(0); //this method to stop any motors, range -1 to 1
+        leftDrive.setPower(0);
+        verticalLift.setPower(0);
+        horizontalLift.setPower(0);
     }
 
 
