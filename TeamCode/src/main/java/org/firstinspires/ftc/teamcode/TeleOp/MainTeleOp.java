02a1eecc082ea18824f3e0b966357b3be36f1a26
Ishaan Javali <ishaanjav@gmail.com>
2019-10-01 17:27:42 -0500

    There were " "s. ;)

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOp/MainTeleOp.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOp/MainTeleOp.java
@@ -58,8 +58,8 @@ public class MainTeleOp extends OpMode {
 
         telemetry.addData("Left Drive Position", robot.leftDrive.getCurrentPosition());
         telemetry.addData("Right Drive Position", robot.rightDrive.getCurrentPosition());
-        telemetry.addData("Left Drive Power", robot.leftDrive.getPower());
-        telemetry.addData("Right Drive Power", robot.rightDrive.getPower());
+        //telemetry.addData("Left Drive Power", robot.leftDrive.getPower());
+        //telemetry.addData("Right Drive Power", robot.rightDrive.getPower());
 
         //telemetry.addData("bucket position", robot.scoopServo.getPosition());
 
